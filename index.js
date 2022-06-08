a5cc85d59b02c2b54209d9b0fa6545d1c6ec9d8a
Charles Jarvis <csjarvis43@gmail.com>
2022-06-07 18:20:36 -0600

    commit tree 26ff127379652f6acec9e46cf362f5cb7d83fcf3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+const destructivelyRemoveLastCat = () => cats.pop()
+
+const destructivelyRemoveFirstCat = () => cats.shift()
+
+function appendCat(name){
+    const endCat = [...cats, name]
+    return endCat
+}
+
+function prependCat(name){
+    const startCat = [name, ...cats]
+    return startCat
+}
+
+function removeLastCat(){
+    const minuslast = (cats.slice(0, cats.length - 1))
+    return minuslast
+}
+
+function removeFirstCat(){
+    const minusFirst = (cats.slice(1, 3))
+    return minusFirst
+}
